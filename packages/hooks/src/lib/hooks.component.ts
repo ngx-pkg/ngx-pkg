51121800d71b240e1be23cc546541938615fc0ac
xunjianxiang <xunjianxiang@outlook.com>
2024-07-08 16:53:30 +0800

    init hooks library

---
+++ packages/hooks/src/lib/hooks.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'x-hooks',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      hooks works!
+    </p>
+  `,
+  styles: ``
+})
+export class HooksComponent {
+
+}
---
+++ packages/hooks/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of hooks
+ */
+
+export * from './lib/hooks.service';
+export * from './lib/hooks.component';
